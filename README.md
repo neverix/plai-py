8fb3e105215c72e1766b952533d5b9d46db233a0
neverix <nev@neverix.io>
2022-07-05 20:17:38 +0300

    Update README.markdown

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # plai-py
 Play Minecraft with AI
+
+## What is this
+A port of OpenAI Video Pretraining for the real Minecraft.
+It isn't a real mod and instead controls the mouse/keyboard for you.
+
+## How to run
+Install:
+
+`pip install -r requirements.txt`
+
+Run:
+
+`python main.py`
+
+## TODO
+
+- [ ] Get user feedback
+- [ ] Make a proper CLI
+- [ ] Remove the CLI and make a GUI
+- [ ] Port to ONNX and make it a proper mod
--- vpt/policy.py
+++ vpt/policy.py
@@ -7,13 +7,13 @@ from gym3.types import DictType
 from torch import nn
 from torch.nn import functional as F
 
-from lib.action_head import make_action_head
-from lib.action_mapping import CameraHierarchicalMapping
-from lib.impala_cnn import ImpalaCNN
-from lib.normalize_ewma import NormalizeEwma
-from lib.scaled_mse_head import ScaledMSEHead
-from lib.tree_util import tree_map
-from lib.util import FanInInitReLULayer, ResidualRecurrentBlocks
+from vpt.action_head import make_action_head
+from vpt.action_mapping import CameraHierarchicalMapping
+from vpt.impala_cnn import ImpalaCNN
+from vpt.normalize_ewma import NormalizeEwma
+from vpt.scaled_mse_head import ScaledMSEHead
+from vpt.tree_util import tree_map
+from vpt.util import FanInInitReLULayer, ResidualRecurrentBlocks
 
 
 class ImgPreprocessing(nn.Module):
